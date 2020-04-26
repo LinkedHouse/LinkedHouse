620269a838965034e8365e685742be0f79adcf56
Gary Ouyang <zouyang@ucsd.edu>
2020-04-25 22:22:54 -0700

    fixed posting error. reorganized layout.

--- LinkedHouse/NewPostViewController.swift
+++ LinkedHouse/NewPostViewController.swift
@@ -2,7 +2,7 @@
 //  NewPostViewController.swift
 //  LinkedHouse
 //
-//  Created by Gary Ouyang on 4/23/20.
+//  Created by Gary Ouyang on 4/25/20.
 //  Copyright © 2020 angela. All rights reserved.
 //
 
@@ -10,38 +10,38 @@ import UIKit
 import AlamofireImage
 import Parse
 
-class NewPostViewController: UIViewController, UIImagePickerControllerDelegate,
-UINavigationControllerDelegate{
+class NewPostViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-    @IBOutlet weak var postimage: UIImageView!
+    @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var postTitle: UITextField!
+    
     @IBOutlet weak var postDescription: UITextField!
     
-    @IBAction func cancelButton(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
+    @IBAction func onCancel(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
-    @IBAction func postButton(_ sender: Any) {
-        let post = PFObject(className:"Posts")
-        let imageData = postimage.image!.pngData()
+    @IBAction func onPost(_ sender: Any) {
+        let post = PFObject(className: "Posts")
+        let imageData = imageView.image!.pngData()
         let file = PFFileObject(data: imageData!)
         
-        post["Title"] = postTitle.text
+        post["Title"] = postTitle.text!
         post["Image"] = file
-        post["Description"] = postDescription.text
-        
+        post["Description"] = postDescription.text!
         post.saveInBackground { (success, error) in
-            if success {
+            if(success){
                 self.dismiss(animated: true, completion: nil)
             } else {
-                print(error)
+                print("error")
             }
         }
     }
+    
     @IBAction func onCameraButton(_ sender: Any) {
         let picker = UIImagePickerController()
         picker.delegate = self
@@ -54,6 +54,7 @@ UINavigationControllerDelegate{
         }
         
         present(picker, animated: true, completion: nil)
+        
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
@@ -61,8 +62,7 @@ UINavigationControllerDelegate{
         let size = CGSize(width: 300, height: 300)
         let scaled = image.af_imageScaled(to: size)
         
-        postimage.image = scaled
-        
+        imageView.image = scaled
         dismiss(animated: true, completion: nil)
     }
     /*
